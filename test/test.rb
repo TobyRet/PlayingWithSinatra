c258f715bb3a7891a0046c9840e2f1f384c16bde
Toby Retallick <toby.retallick@gmail.com>
2015-01-19 12:58:48 +0000

    Fixed broken test

--- test/test.rb
+++ test/test.rb
@@ -2,7 +2,7 @@ ENV['RACK_ENV'] = 'test'
 require 'sinatra'
 require 'minitest/autorun'
 require 'rack/test'
-require_relative 'number_cruncher.rb'
+require_relative '../number_cruncher.rb'
 
 include Rack::Test::Methods
 
